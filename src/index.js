4cc6f2d46d3a6c5a7e679b32f500e70ace7b5c28
Vitalii <vetall4444@gmail.com>
2020-06-09 10:25:39 +0500

    init

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import './js/script'
+import './scss/main.scss'
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "/dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+          }, {
+            loader: 'sass-loader',
+          }
+        ]
+      }
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+}
